142563b6d393391d233040b3809f3b6af28358ce
Aaron <aaronkh19@gmail.com>
2020-07-26 20:32:50 -0600

    finished routes, but not working correctly

---
+++ models/workout.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: "Please insert the name of the workout",
+  },
+  type: {
+    type: String,
+    trim: true,
+    required: "Please insert the type of exercise you plan to do",
+  },
+  weight: {
+    type: Number,
+    required: "Please insert how much weight you plan to lift",
+  },
+  sets: {
+    type: Number,
+    required: "Please insert how many sets you plan to do",
+  },
+  reps: {
+    type: Number,
+    required: "Please insert how many reps you plan to do",
+  },
+  duration: {
+    type: Number,
+    required: "Please insert how long you plan to workout",
+  },
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
---
+++ routes/api.js
@@ -0,0 +1,35 @@
+const router = require("express").Router();
+const Workout = require("../models/workout.js");
+const { db } = require("../models/workout.js");
+
+router.post("/api/workout", ({ body }, res) => {
+  Workout.create(body)
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+router.put("/api/workout/:id", ({ body, params }, res) => {
+  Workout.updateOne({ _id: params.id }, { $push: { Workout: body } })
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+router.get("/api/workout", (req, res) => {
+  Workout.find({})
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+module.exports = router;
